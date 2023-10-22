487fbed85a001fe0a6a629331d6a815a9c70fec2
nicbohm <nicliambohm@gmail.com>
2023-10-22 18:30:12 +0200

    Hello World

---
+++ src/main/java/htwberlin/focustimer/HelloController.java
@@ -0,0 +1,12 @@
+package htwberlin.focustimer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
