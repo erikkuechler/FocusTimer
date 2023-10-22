fac824965474a354c3bc891de3921cbe6b4be7be
Erik Küchler <erik.kuechler@student.htw-berlin.de>
2023-10-22 18:17:18 +0200

    spring setup

---
+++ src/main/java/htwberlin/focustimer/FocusTimerApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.focustimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusTimerApplication.class, args);
+	}
+
+}
