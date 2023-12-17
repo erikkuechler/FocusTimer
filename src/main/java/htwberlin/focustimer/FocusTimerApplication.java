b61eaa93e1e8523de3e9cf0a6f5de584be3e90dd
Erik Küchler <erik.kuechler@student.htw-berlin.de>
2023-12-17 02:32:42 +0100

    Standard products added

--- src/main/java/htwberlin/focustimer/FocusTimerApplication.java
+++ src/main/java/htwberlin/focustimer/FocusTimerApplication.java
@@ -19,8 +19,16 @@ public class FocusTimerApplication {
 	public CommandLineRunner demo(ProductRepository repository) {
 		return (args) -> {
 		// save a few products
-		repository.save(new Product("Default", 1, "Foreground", "Baum_Default.png"));
-		repository.save(new Product("Weihnachtsbaum", 10, "Foreground", "Baum_Christmas.png"));
+		if (repository.count() == 0) {
+			// Foreground
+			repository.save(new Product("Baum", 3, "Foreground", "Baum_Default"));
+			repository.save(new Product("Apfelbaum", 5, "Foreground", "Baum_Apple"));
+			repository.save(new Product("Weihnachtsbaum", 10, "Foreground", "Baum_Christmas"));
+			// Background
+			repository.save(new Product("Blau", 3, "Background", "bg-blue"));
+			repository.save(new Product("Gold", 8, "Background", "bg-gold"));
+			repository.save(new Product("Premium Grau", 15, "Background", "bg-gray"));
+		}
 		};
 	}
 
